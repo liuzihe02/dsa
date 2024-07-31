87f4213d6e65e7935204c8e1c955691b07d55193
flowingpurplecrane <liuzihenanhua@gmail.com>
2024-07-31 15:42:23 +0100

    hashing, TreeMap

--- graph.py
+++ graph.py
@@ -2,18 +2,35 @@
 from graphviz import Graph, Source
 
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, key=None,val=0,left=None,right=None):
+        #this is for tree map
+        self.key=key
+        #this is used for all BSTs
         self.val = val
+        #by default left and right child nodes are initalized to None
         self.left = left
         self.right = right
 
-def visualize_tree(root):
-    '''assumes root is the root node of a Binary Tree
+from graphviz import Graph
+
+def visualize_tree(root, TreeMap=False):
+    '''
+    Visualizes a Binary Tree or a TreeMap, taking in the root node as a starting point
     
-    return a graph object'''
+    :param root: The root node of the tree
+    :param TreeMap: Boolean indicating if the tree is a TreeMap (default: False)
+        if its a TreeMap, the keys are ordered, and each node stores a key and a value
+    :return: A graph object
+    '''
     def add_nodes_edges(node):
         if node:
-            g.node(str(id(node)), str(node.val))
+            if TreeMap:
+                node_label = f"Key {node.key}: Value {node.val}"
+            else:
+                node_label = str(node.val)
+            
+            g.node(str(id(node)), node_label)
+            
             if node.left:
                 g.edge(str(id(node)), str(id(node.left)))
                 add_nodes_edges(node.left)
